19f03f79a0a052fad04e5437b55061904dfef0ea
sixtick_samsung <sixtick65@gmail.com>
2026-03-22 19:08:44 +0900

    레이아웃 정리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,48 @@
-import { LoadingMark } from "../Icons";
-import { useState, useEffect } from "react";
-import Login from "./Login";
+// import { LoadingMark } from "../Icons";
+// import { useState, useEffect } from "react";
+
 
 const Home = () => {
-    const [count, setCount ] = useState(0);
-    const [loading, setLoading] = useState<boolean>(false);
+    // const [count, setCount ] = useState(0);
+    // const [loading, setLoading] = useState<boolean>(false);
 
-    const getCount = async () => {
-        setLoading(true);
-        try {
-            const res = await fetch('http://localhost:3000/count')
-            if(res.ok){
-                const data = await res.json();
-                console.log(data);
-                setCount(data.test_count);
-            }
+    // const getCount = async () => {
+    //     setLoading(true);
+    //     try {
+    //         const res = await fetch('http://localhost:3000/count')
+    //         if(res.ok){
+    //             const data = await res.json();
+    //             console.log(data);
+    //             setCount(data.test_count);
+    //         }
             
-        }catch(error){
-            console.log(error);
-        }finally{
-            setLoading(false);
-        }
-    }
-    const incrementCount = async () => {
-        try {
-            const res = await fetch('http://localhost:3000/count/increment', {
-                method: 'POST'
-            });
-            if(res.ok){
-                const data = await res.json();
-                console.log('incrementCount', data);
-                setCount(data.test_count);
-            }
+    //     }catch(error){
+    //         console.log(error);
+    //     }finally{
+    //         setLoading(false);
+    //     }
+    // }
+    // const incrementCount = async () => {
+    //     try {
+    //         const res = await fetch('http://localhost:3000/count/increment', {
+    //             method: 'POST'
+    //         });
+    //         if(res.ok){
+    //             const data = await res.json();
+    //             console.log('incrementCount', data);
+    //             setCount(data.test_count);
+    //         }
             
-        }catch(error){
-            console.log(error);
-        }finally{
-        }
-    }
+    //     }catch(error){
+    //         console.log(error);
+    //     }finally{
+    //     }
+    // }
     
-    // 컴포넌트 시작시 실행
-    useEffect(() => {
-        getCount();
-    }, []);
+    // // 컴포넌트 시작시 실행
+    // useEffect(() => {
+    //     getCount();
+    // }, []);
 
 
     return (
